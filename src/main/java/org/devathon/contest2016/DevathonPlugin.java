b116d834e40098781bc7d576aa9c4cb365d6c21e
Scarsz <Scarsz@users.noreply.github.com>
2016-11-05 10:15:37 -0700

    only way in hell I could get the server going

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -6,12 +6,13 @@ public class DevathonPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // put your enable code here
+
     }
 
     @Override
     public void onDisable() {
-        // put your disable code here
+
     }
+
 }
 
